75a1831986b860e6ffa211a6549bb9be6fea5bb8
Mark L. Chang <mark.chang@gmail.com>
2011-04-06 15:03:54 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopshop::Application.config.secret_token = 'ae2677650e427cf683137f0b67347f903dfa9f79e1cc4416f1765e2b830ce013179084736be36828b8594aa5ef77df032666edb2ad9fe60bd087920912609ed7'
